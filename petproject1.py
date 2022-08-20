fcbbb9d76aa72e4c5d483e0b49a398cbc1e32056
TimurShakirovWW <timurchivich@gmail.com>
2022-08-20 18:05:24 +0500

    PetProject1
    PetProject1

---
+++ petproject1.py
@@ -0,0 +1,15 @@
+main = int(input("номер месяца:"))
+
+if main == 1 or main == 2 or main == 12:
+    print("зима")
+elif main == 3 or main == 4 or main == 5:
+    print("весна")
+elif main == 9 or main == 10 or main == 11:
+    print("осень")
+elif main == 6 or main == 7 or main == 8:
+    print("лето")
+
+
+
+
+
